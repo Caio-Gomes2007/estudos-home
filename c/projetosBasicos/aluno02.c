4a508ea522f211c3ab6762a7452ec9615f453522
Caio Gomes <caiofgomesrj@gmail.com>
2024-04-21 10:56:06 -0300

    add email
    adicionando o email

--- c/projetosBasicos/aluno02.c
+++ c/projetosBasicos/aluno02.c
@@ -6,6 +6,7 @@ struct aluno
 {
     int idade;
     char nome[50];
+    char email[50];
 };
 typedef struct aluno aluno;
 
@@ -18,6 +19,8 @@ void add(aluno listadealunos[], int *numAluno)
         scanf("%s", listadealunos[*numAluno].nome);
         printf("Digite a idade do aluno: ");
         scanf("%i", &listadealunos[*numAluno].idade);
+        printf("Digite seu email: ");
+        scanf("%s", listadealunos[*numAluno].email);
         (*numAluno)++;
         printf("Aluno adicionado com sucesso!\n");
     }
@@ -34,6 +37,7 @@ void ler(aluno listadealunos[], int numAluno)
         printf("Aluno %i\n", i + 1);
         printf("nome: %s\n", listadealunos[i].nome);
         printf("idade: %i\n", listadealunos[i].idade);
+        printf("email: %s\n", listadealunos[i].email);
         printf("\n");
     }
 }
@@ -54,46 +58,80 @@ void deletar(aluno listadealunos[], int *numAluno)
     }
     (*numAluno)--;
 }
-void editar(aluno listadealunos[],int *numAluno){
-    int index02;
+void editar(aluno listadealunos[], int *numAluno)
+{
+    int index;
     printf("Digite o número do aluno que deseja editar: ");
-    scanf("%i", &index02);
+    scanf("%i", &index);
 
-    if (index02 < 1 || index02 > *numAluno) {
+    if (index < 1 || index > *numAluno)
+    {
         printf("Erro: Índice inválido.\n");
-    } else {
+    }
+    else
+    {
         printf("Digite o novo nome do aluno: ");
-        scanf("%s", listadealunos[index02 - 1].nome);
+        scanf("%s", listadealunos[index - 1].nome);
         printf("Digite a nova idade do aluno: ");
-        scanf("%i", &listadealunos[index02 - 1].idade);
+        scanf("%i", &listadealunos[index - 1].idade);
+        printf("Digite o novo email: ");
+        scanf("%s", listadealunos[index - 1].email);
         printf("Aluno editado com sucesso!\n");
     }
-}void buscar_nome(aluno listadealunos[],int numAluno){
+}
+void buscar_nome(aluno listadealunos[], int numAluno)
+{
     char possivelNome[50];
     int alunoAchado = 0;
-       printf("digite o nome dele \n");
-             scanf("%s",possivelNome);
-             
-             for(int i =0;i<numAluno;i++){
-                 if(strcmp(listadealunos[i].nome,possivelNome)==0){
-                     printf("achamos:\n");
-                     printf("%s\n",listadealunos[i].nome);
-                     printf("%i\n",listadealunos[i].idade);
-                     alunoAchado =1;
-                      }
-             }if(alunoAchado==0){
-                 printf("erro ao procurar,não achado");
-             }
+    printf("digite o nome dele \n");
+    scanf("%s", possivelNome);
 
+    for (int i = 0; i < numAluno; i++)
+    {
+        if (strcmp(listadealunos[i].nome, possivelNome) == 0)
+        {
+            printf("\nachamos:\n");
+            printf("%s\n", listadealunos[i].nome);
+            printf("%i\n", listadealunos[i].idade);
+            printf("%s\n", listadealunos[i].email);
+            alunoAchado = 1;
+        }
+    }
+    if (alunoAchado == 0)
+    {
+        printf("erro ao procurar,não achado");
+    }
+}
+void buscar_email(aluno listadealunos[], int numAluno)
+{
+    char possivelemail[50];
+    int alunoAchado = 0;
+    printf("digite o email dele \n");
+    scanf("%s", possivelemail);
+
+    for (int i = 0; i < numAluno; i++)
+    {
+        if (strcmp(listadealunos[i].email, possivelemail) == 0)
+        {
+            printf("\nachamos:\n");
+            printf("%s\n", listadealunos[i].nome);
+            printf("%i\n", listadealunos[i].idade);
+            printf("%s\n", listadealunos[i].email);
+            alunoAchado = 1;
+        }
+    }
+    if (alunoAchado == 0)
+    {
+        printf("erro ao procurar,não achado");
+    }
 }
 int main()
 {
 
     int tipoOpe;
     int tipoOpe02;
-    int index02;
     int numAluno = 0;
-    
+
     aluno listadealunos[MAX_ALUNOS];
     do
     {
@@ -101,8 +139,8 @@ int main()
         printf("1 - Cadastrar aluno\n");
         printf("2 - Ver alunos cadastrados\n");
         printf("3 - Editar aluno\n");
-        printf("4 - Deletar alunos\n");
-        printf("5- buscar aluno\n");
+        printf("4 - buscar alunos\n");
+        printf("5 - Deletar aluno\n");
         printf("6 - Sair\n");
         printf("Escolha uma opção: ");
         scanf("%i", &tipoOpe);
@@ -115,25 +153,26 @@ int main()
             ler(listadealunos, numAluno);
             break;
         case 3:
-            editar(listadealunos,&numAluno);
+            editar(listadealunos, &numAluno);
             break;
         case 4:
-            deletar(listadealunos, &numAluno);
-    break;
-        case 5:
             printf("pesquisa por nome(1) ou e-mail?(2) \n");
             scanf("%i", &tipoOpe02);
-            switch( tipoOpe02){
-         case 1:
-            buscar_nome(listadealunos,numAluno);
-        break;
-        case 2:
-        printf ("ainda não implementado\n"); 
-        break;
-        default:
-        printf("erro na seleçao\n");
-        break;
-        }
+            switch (tipoOpe02)
+            {
+            case 1:
+                buscar_nome(listadealunos, numAluno);
+                break;
+            case 2:
+                buscar_email(listadealunos, numAluno);
+                break;
+            default:
+                printf("erro na seleçao\n");
+                break;
+            }
+        case 5:
+            deletar(listadealunos, &numAluno);
+            break;
         case 6:
             printf("\nSaindo\n");
             break;
